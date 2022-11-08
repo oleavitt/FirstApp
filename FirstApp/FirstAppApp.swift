24780e451e7e4451f290b2dbd3aa7738fdbd20f5
Oren Leavitt <oleavitt@ix.netcom.com>
2022-11-08 12:16:21 -0600

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Oren Leavitt on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
